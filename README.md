1bd058a984aac64d1cc54e9c4e0814c465e4fe6b
Pitt Wu <kgb00128@gmail.com>
2022-04-09 15:49:12 +0800

    chore: init my task use javascript

---
+++ README.md
@@ -0,0 +1,3 @@
+# Todos App
+
+> use vanilla js build todo list, and provide for use by tutors.
--- js/v7.js
+++
@@ -1,9 +0,0 @@
-var newName = { name: 'Nancy' };
-var name = 'Pitt'
-
-function sayMyName() {
-  console.log(this.name)
-}
-
-var newFunc = sayMyName.bind(newName)
-newFunc() // 印出 Nancy
